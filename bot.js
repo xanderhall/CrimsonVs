863e8a547d2217152d3fdf179ac5fce2d05fd9e8
xanderhall <mattgr2010@gmail.com>
2019-11-27 02:37:28 -0330

    Add help command

--- bot.js
+++ bot.js
@@ -1,20 +1,42 @@
 const Discord = require('discord.io');
-const auth = require("./auth.json");
+const auth = require('./auth.json');
 
-var bot = new Discord.Client({
+const bot = new Discord.Client({
     autorun: true,
-    token: auth.token
+    token: auth.token,
 });
 
-bot.on('ready', function(event) {
+const help = new Map([
+    ['force', 'A card that uses the term "force" indicates an effect that overrides the usual flow of the game. If both Generals have forced effects that conflict, they cancel each other out.'],
+    ['nullify', 'This keyword indicates that the effect specified by the card is nullified, as well any effects that would normally occur as a result.'],
+]);
+
+bot.on('ready', () => {
     console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
-bot.on('message', function(user, userID, channelID, message, event) {
-    if (message === "ping") {
+bot.on('message', (user, userID, channelID, message, event) => {
+    if (message === 'ping') {
         bot.sendMessage({
             to: channelID,
-            message: "pong"
+            message: 'pong',
         });
+    }
+    // Command
+    if (message.startsWith('!')) {
+        if (message.startsWith('!help')) {
+            const topic = message.split(' ')[1].toLowerCase();
+            if (help.has(topic)) {
+                bot.sendMessage({
+                    to: channelID,
+                    message: help.get(topic),
+                });
+            } else {
+                bot.sendMessage({
+                    to: channelID,
+                    message: `No help available for ${topic}`,
+                });
+            }
+        }
     }
 });
